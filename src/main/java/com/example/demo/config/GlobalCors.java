b84765595b164e58ff66061a8156475116775f47
betinsk <gmdust@hotmail.com>
2024-03-30 23:10:31 -0300

    Add cors configuração

---
+++ src/main/java/com/example/demo/config/GlobalCors.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCors implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Permitir solicitações do domínio do React
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
