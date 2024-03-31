33723299c3477bb18c1b5e886aaddf46f02c6797
betinsk <gmdust@hotmail.com>
2024-03-30 21:44:48 -0300

    H2console

---
+++ src/main/java/com/example/demo/domain/Imate.java
@@ -0,0 +1,83 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Imate implements Serializable{
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer age;
+	private String gender;
+	private String name;
+	
+	public Imate() {
+		
+	}
+
+	public Imate(Integer id, Integer age, String gender, String name) {
+		super();
+		this.id = id;
+		this.age = age;
+		this.gender = gender;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Imate other = (Imate) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
--- src/main/java/com/example/demo/resources/ImateResource.java
+++ src/main/java/com/example/demo/resources/ImateResource.java
@@ -1,16 +1,29 @@
 package com.example.demo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Imate;
 
 @RestController
 @RequestMapping(value="/imates")
 public class ImateResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar() {
-		return "REST ESTÁ FUNCIONANDO";
+	public List<Imate> Listar() {
+		
+		Imate imt = new Imate(1, 56, "Male", "Whiter White");
+		Imate imt2 = new Imate(2, 23, "Male", "Jessy Pinkman");
+		List<Imate> list = new ArrayList<>();
+		
+		list.add(imt);
+		list.add(imt2);
+
+		return list;
 	}
 	
 }
